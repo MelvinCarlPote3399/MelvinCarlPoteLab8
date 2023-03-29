54b5691ea0f96616907e56aea52e63b39815cdbd
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-03-29 18:42:57 -0400

    Implemented the full bottom navigation menu; have yet to add any functionality to the respective fragments.

--- MelvinModule8/src/main/java/melvincarl/pote/n01483399/melvinlayout8/PoteActivity.java
+++ MelvinModule8/src/main/java/melvincarl/pote/n01483399/melvinlayout8/PoteActivity.java
@@ -1,14 +1,48 @@
 package melvincarl.pote.n01483399.melvinlayout8;
 
+import java.io.*;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+//import com.example.Fragment.*;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.os.Bundle;
+import android.view.MenuItem;
 
 import android.os.Bundle;
 
-public class PoteActivity extends AppCompatActivity {
+public class PoteActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
     }
+
+    HomeFragment homeFragment = new HomeFragment();
+    PoteFragment poteFragment = new PoteFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,homeFragment).commit();
+                return true;
+
+            case R.id.melvin:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,poteFragment).commit();
+                return true;
+        }
+
+        return false;
+    }
+
 }
