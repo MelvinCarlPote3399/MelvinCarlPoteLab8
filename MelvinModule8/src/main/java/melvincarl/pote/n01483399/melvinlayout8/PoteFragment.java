b0c5bb95db46dc09730ce8c0398eeff1ac1ff16f
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-03-30 10:28:35 -0400

    Added the implementation for the final screen; data was successfully passed into the textview.

--- MelvinModule8/src/main/java/melvincarl/pote/n01483399/melvinlayout8/PoteFragment.java
+++ MelvinModule8/src/main/java/melvincarl/pote/n01483399/melvinlayout8/PoteFragment.java
@@ -1,15 +1,24 @@
 package melvincarl.pote.n01483399.melvinlayout8;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class PoteFragment extends Fragment {
@@ -24,8 +33,69 @@ public class PoteFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_pote, container, false);
 
-        //Button readCourses;
+        Button readCourses = view.findViewById(R.id.readCButton);
+        Button readFile = view.findViewById(R.id.readFButton);
+        TextView dataField = view.findViewById(R.id.secondFragment);
 
+        readCourses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                String coursesJson = preferences.getString("courses",null);
+                dataField.setText("");
+
+                if(coursesJson != null){
+                    Gson gson = new Gson();
+                    String [] courses = gson.fromJson(coursesJson,String[].class);
+
+                    String textCourse = "";
+                    for (String course : courses){
+                        textCourse += course;
+                    }
+                    dataField.setText(textCourse);
+                }
+                else {
+                    dataField.setText(getResources().getString(R.string.no_data));
+                }
+
+            }
+        });
+
+        readFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataField.setText("");
+                File filePath = getContext().getFilesDir();
+                File file = new File(filePath,"melvin.txt");
+
+                try{
+                    FileInputStream fileInputStream = new FileInputStream(file);
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+
+                    while ((line = bufferedReader.readLine()) != null){
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    fileInputStream.close();
+
+                    String contents = stringBuilder.toString();
+
+                    if (contents.length() == 0){
+                        dataField.setText(getResources().getString(R.string.no_data));
+                    }
+                    else {
+                        dataField.setText(contents);
+                    }
+
+                }
+                catch (IOException exception){
+                    exception.printStackTrace();
+                }
+
+            }
+        });
 
 
         return view;
